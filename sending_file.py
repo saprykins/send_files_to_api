9f3823dc460760770e0490a64daeb31626791460
saprykins <palatatot@gmail.com>
2022-03-26 21:37:24 +0100

    function that sends a file to api and gets text in response is added

--- sending_file.py
+++ sending_file.py
@@ -1,52 +1,29 @@
 import requests
 
-# we can check w/ this api
-# test_url = "http://httpbin.org/post"
 
-# it's my api
-# is used to send pdf-file and get document_id
-post_url = "http://localhost:5000/documents"
+def get_text_from_api(file_url):
 
-files = {"file": open("sample.pdf", "rb")}
+    # it's the internal api 
+    # is used to send pdf-file and get document_id
+    post_url = "http://localhost:5000/documents"
 
-# send file
-response = requests.post(post_url, files=files)
+    files = {"file": open(file_url, "rb")}
 
-# get id of the document saved in database
-document_id = response.json()['id']
+    # send file
+    response = requests.post(post_url, files=files)
+    
+    # api is configured to return documenet_id
+    # get id of the document saved in database
+    document_id = response.json()['id']
 
-test_get = "http://localhost:5000/text/"+str(document_id)+".txt"
+    # getting data from api
+    get_url = "http://localhost:5000/text/"+str(document_id)+".txt"
 
-# get data
-# payload = {'id': '55'}
-response = requests.get(test_get)
-# print(response.text['text'])
-dict = response.json()
-print(dict['text'])
+    # the api is configured to return text when document_id is sent
+    response = requests.get(get_url)
+    # print(response.text['text'])
+    text_from_file = response.json()
 
+    return text_from_file['text']
 
-
-
-"""
-test_file = open("sample.pdf", "rb")
-test_response = requests.post(test_url, files = {"form_field_name": test_file})
-print(test_response)
-"""
-
-
-'''
-if test_response.ok:
-    print(test_response.text)
-'''
-
-'''
-files = {
-    'file': ('sample.pdf', open('sample.pdf', 'rb')),
-}
-
-files = {
-    "file": open("sample.pdf", "rb"),
-    # "file": open("sample_2.pdf", "rb"),
-    # "file": open("sample_3.pdf", "rb"),
-}
-'''
+print(get_text_from_api('sample.pdf'))
